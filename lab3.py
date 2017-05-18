7f64e031334e824f24f33d12a9ea9e9bb47e3e69
Alois Klink <alois.klink@gmail.com>
2017-05-18 12:23:11 +0100

    Fixes bugs in formatting.
    lab3.py is now perfectly designed to be published as a pdf using
    pweave, by running the command:
    
    `pypublish -f pdf lab3.py`

--- lab3.py
+++ lab3.py
@@ -12,20 +12,16 @@
 
 from simulateChemicals import *
 
-#' $ \varnothing \xrightarrow{1} X $
+#' Here are the reaction formulas:
+#'
+#'* $\emptyset \overset{1}{\to} X$
+#'* $X \overset{2}{\to} Y$
+#'* $2 X + Y \overset{0.02}{\to} 3 X$
+#'* $X \overset{0.04}{\to} \emptyset$
 
 reactions = [Reaction(lambda X: 1, [1,0]),
-
-#' $ X \xrightarrow{2} Y $
-
              Reaction(lambda X: 2*X[0], [-1,1]),
-             
-#' $ 2 X + Y \xrightarrow{0.02} 3 X $
-
              Reaction(lambda X: 0.02* X[0]**2 *X[1], [1,-1]),
-             
-#' $ X \xrightarrow{0.04} \varnothing $
-             
              Reaction(lambda X: 0.04*X[0], [-1,0])]
 
 #' # Displaying the ODE
@@ -37,7 +33,7 @@ reactions = [Reaction(lambda X: 1, [1,0]),
 #' converting all Y to X. Once Y runs out, X slowly begins to degrade to
 #' an equilibrium position.
 
-#+ trajectories, caption='ODE Simulation Trajectories from 0 initialConditions'
+#+trajectories, caption='ODE Simulation Trajectories from 0 initialConditions'
 system = ChemicalReactionsSystem(reactions, 2)
 system.trajectories()
 
@@ -49,9 +45,9 @@ system.trajectories()
 #' increasing the number of molecules, and speeding up reactions. However, these
 #' graphs have molecules split into pieces, which is not possible in the real world.
 
-for r in [1]:
-	reactions = Reaction.rescaleReactions(reactions, r)
-	system = ChemicalReactionsSystem(reactions, 2)
+r = 1
+reactions = Reaction.rescaleReactions(reactions, r)
+system = ChemicalReactionsSystem(reactions, 2)
 	
 #' This shows how the cocentration changes over time. Notice that due the high
 #' randomness of the properties, the threshold point is reached much faster. As
@@ -59,11 +55,27 @@ for r in [1]:
 #' gets smaller and smaller, so that the threshold point is reached at the same
 #' time as the ODE.
 
-	print("r is " + str(r))
-	system.gillespieConcentrations(500*r)
+print("r is " + str(r))
+system.gillespieConcentrations(50000*r)
 
 #' This shows how the cocentration X changes in relation to the concentrations Y.
 #' Notice that due the high randomness of the properties, the path is a lot tighter,
 #' and the stable point seems to be a lot lower.
-	system.gillespieTrajectories([[0, 0], [8, 8], [4, 23]],
-			                     10000*r)
+
+system.gillespieTrajectories([[0, 0], [4, 23]],
+		                     10000*r)
+		                     
+#' # Reduction
+
+#' The following graphs have r = 10
+
+r = 10
+reactions = Reaction.rescaleReactions(reactions, r)
+system = ChemicalReactionsSystem(reactions, 2)
+
+#+caption='r = 10', width="15cm"
+system.gillespieConcentrations(10000*r)
+
+#+caption='r = 10', width="15cm"
+system.gillespieTrajectories([[0, 0], [4, 23]],
+		                     10000*r)
