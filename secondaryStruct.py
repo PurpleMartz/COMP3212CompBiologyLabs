6a93f12e348e562f1eeea140f5782c9edfaf19a5
Alois Klink <alois.klink@gmail.com>
2017-05-19 05:25:49 +0100

    Upgrades MLP function to work better.
    Adds penalty for large weights.
    Also adds validation dataset.

--- secondaryStruct.py
+++ secondaryStruct.py
@@ -119,102 +119,154 @@ def loadSecondaryStructFASTAs(fileName, limit = 2000, seqLengthLimit = 2000):
 			if numberFound >= limit:
 				return secondaryStructFASTAs
 
-class SecondaryStructSolver(object):
-	def runMLP(self, trainingFASTAs, testingFASTAs):
-		learning_rate = 0.001
-		training_epochs = 15
-		display_step = 1
+def secondaryStructSolverMLP(trainingFASTAs, testingFASTAs, layerSizes = [50, 50, 50],
+           training_epochs = 100, batch_size = 100, learning_rate = 0.1,
+           seperateValidationAndTest = True, validationPercentage = 0.2,
+           weightCost = 0.05):
+	"""Runs a Multi-layer Perceptron Neural Network.
 	
-		# Network Parameters
-		n_hidden_1 = 500 # 1st layer number of features
-		n_hidden_2 = 1000 # 2nd layer number of features
-		n_input = 17*22 # size of the input (window size 17, 21 entries)
-		n_classes = 4 # total classes (H, C, E, and X)
+	Trains with the trainingFASTAs data, and tests it with
+	the testingFASTAs data.
 	
-		# tf Graph input
-		x = tf.placeholder(tf.float32, [None, n_input])
-		y = tf.placeholder(tf.float32, [None, n_classes])
-	
-		# Create model
-		def multilayer_perceptron(x, weights, biases):
-			# Hidden layer with RELU activation
-			layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-			layer_1 = tf.nn.relu(layer_1)
-			# Hidden layer with RELU activation
-			layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-			layer_2 = tf.nn.relu(layer_2)
-			# Output layer with linear activation
-			out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-			return out_layer
-	
-		# Store layers weight & bias
-		weights = {
-			'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-			'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-			'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
-		}
-		biases = {
-			'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-			'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-			'out': tf.Variable(tf.random_normal([n_classes]))
-		}
-	
-		# Construct model
-		pred = multilayer_perceptron(x, weights, biases)
-
-		# Define loss and optimizer
-		cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-		optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-		# Initializing the variables
-		init = tf.global_variables_initializer()
+	Code loosely based on the code found here:
+		https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/multilayer_perceptron.py
 		
-		# Launch the graph
-		with tf.Session() as sess:
-			sess.run(init)
+	Args:
+		trainingFASTAs: A list of SecondaryStructFASTA objects to train with
+		testingFASTAs: A list of SecondaryStructFASTA objects to test with
+		layerSizes: A list of the number of neurons wanted in each layer
+		training_epochs: How long training should go on for
+		batch_size: How many sequences will be trained each epoch
+		learning_rate: The learning rate of the ADAM optimizer
+		seperateValidationAndTest: if True, creates a validation from training 
+		                           data, otherwise uses testing data
+		validationPercentage: percent of batch_size that should be used
+		weightCost: how much having large weights is punished
+	"""
+	display_step = 1
+	
+	validationFASTAs = []
+	if (seperateValidationAndTest):
+		validationEnd = int( len(trainingFASTAs) * validationPercentage )
+		shuffle = random.sample(range(len(trainingFASTAs)), len(trainingFASTAs))
+		# splits training data into training and validation data
+		validationFASTAs = trainingFASTAs[:validationEnd]
+		trainingFASTAs = trainingFASTAs[validationEnd:]
+	else:
+		validationFASTAs = testingFASTAs
 
-			# Training cycle
-			print("Started training at: " + str(datetime.now()))
-			print("Press CTRL+C to stop early")
-			try:
-				for epoch in range(training_epochs):
-					avg_cost = 0.
-					total_batch = 100
-					# Loop over all batches
-					for i in random.sample(range(len(trainingFASTAs)), total_batch):
-						batch_x, batch_y = trainingFASTAs[i].createInputOutput()
-						# Run optimization op (backprop) and cost op (to get loss value)
-						_, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
-								                                      y: batch_y})
-						# Compute average loss
-						avg_cost += c / total_batch
-					# Display logs per epoch step
-					if epoch % display_step == 0:
-						print("Epoch:", '%04d' % (epoch+1), "cost=", \
-							  "{:.9f}".format(avg_cost))
+	# Network Parameters
+	n_input = 17*22 # size of the input (window size 17, 21 entries)
+	n_classes = 4 # total classes (H, C, E, and X)
+	n_hidden = layerSizes
+
+	# tf Graph input
+	x = tf.placeholder(tf.float32, [None, n_input])
+	y = tf.placeholder(tf.float32, [None, n_classes])
+
+	# Create model
+	def multilayer_perceptron(x, weights, biases):
+		prev_layer = x
+		for i in range(len(n_hidden)):
+			# Hidden layer with RELU activation
+			layer = tf.add(tf.matmul(prev_layer, weights[i]), biases[i])
+			prev_layer = tf.nn.relu(layer)
+
+		# Output layer with linear activation
+		out_layer = tf.matmul(prev_layer, weights[len(n_hidden)]) + biases[len(n_hidden)]
+		return out_layer
+	
+	# Create Weights and Biases for the layers
+	weights = []
+	biases = []
+	prev_layerSize = n_input
+	for layerSize in (n_hidden + [n_classes]):
+		# weights are from previous layer to this layer
+		weights.append(tf.Variable(tf.random_normal([prev_layerSize, layerSize])))
+		# biases are for each neuron
+		biases.append( tf.Variable(tf.random_normal([layerSize])) )
+		prev_layerSize = layerSize
+
+	# Construct model
+	pred = multilayer_perceptron(x, weights, biases)
+
+	# Define loss and optimizer
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+	
+	# penalizes large weights
+	l2_weights_loss = tf.add_n([tf.nn.l2_loss(w) for w in weights])
+	weight_loss_gain = weightCost / len(weights)
+	cost = cost + weight_loss_gain * l2_weights_loss
+	
+	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+	# Initializing the variables
+	init = tf.global_variables_initializer()
+	
+	# Launch the graph
+	with tf.Session() as sess:
+		sess.run(init)
+		
+		# Test model
+		correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+		# Calculate accuracy
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+		# Training cycle
+		print("MLP Neural Net with Neurons: " + str(n_hidden) 
+		      + " weightCost: " + str(weightCost))
+		print("Started training at: " + str(datetime.now()))
+		print("Press CTRL+C to stop early")
+		try:
+			prev_valid_cost = 0.0
+			for epoch in range(training_epochs):
+				avg_cost = 0.
+				# Loop over all batches
+				for i in random.sample(range(len(trainingFASTAs)), batch_size):
+					batch_x, batch_y = trainingFASTAs[i].createInputOutput()
+					# Run optimization op (backprop) and cost op (to get loss value)
+					_, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+							                                      y: batch_y})
+					# Compute average loss
+					avg_cost += c / batch_size
 					
-			except KeyboardInterrupt:
-				pass #if ctrl+c is pressed stop early
-				 
-			print("Optimization Finished!")
-
-			# Test model
-			correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-			# Calculate accuracy
-			accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-		
-			test_x, test_y =  testingFASTAs[0].createInputOutput()
-			testSize = 100
-			for i in range(1, testSize):
-				test_x_i, test_y_i = testingFASTAs[i].createInputOutput()
-				test_x = np.concatenate( (test_x, test_x_i) )
-				test_y = np.concatenate( (test_y, test_y_i) )
-			test_x = np.array(test_x)
-			test_y = np.array(test_y)
-		
-			print("Accuracy:", accuracy.eval({x: test_x, y: test_y}))
+				# Validate Data
+				valid_avg_percentage = 0.
+				for i in random.sample(range(len(validationFASTAs)),
+				                       int( batch_size*validationPercentage ) ):
+					batch_x, batch_y = validationFASTAs[i].createInputOutput()
+					c, = sess.run([accuracy], feed_dict={x: batch_x,
+					                                y: batch_y})
+					valid_avg_percentage += c / int(batch_size*validationPercentage)
+					
+				# Display logs per epoch step
+				if epoch % display_step == 0:
+					print("Epoch:", '%04d' % (epoch+1), " train cost =", \
+						  "{:.9f}".format(avg_cost))
+					print("      correct validations =", \
+						  "{:.9f}".format(100*valid_avg_percentage) + "%")
+						  
+				if(prev_valid_cost >= valid_avg_percentage):
+					print("Overfitting so maybe you should stop")
+					
+				prev_valid_cost = valid_avg_percentage
+				
+		except KeyboardInterrupt:
+			pass #if ctrl+c is pressed stop early
+			 
+		print("Optimization Finished!")
+	
+		test_x, test_y =  testingFASTAs[0].createInputOutput()
+		testSize = 500
+		for i in range(1, testSize):
+			test_x_i, test_y_i = testingFASTAs[i].createInputOutput()
+			test_x = np.concatenate( (test_x, test_x_i) )
+			test_y = np.concatenate( (test_y, test_y_i) )
+		test_x = np.array(test_x)
+		test_y = np.array(test_y)
+	
+		print("Accuracy:", accuracy.eval({x: test_x, y: test_y}))
 			
-trainingFASTAs = loadSecondaryStructFASTAs("data/seq+ss_train.txt", 200)
-testingFASTAs = loadSecondaryStructFASTAs("data/seq+ss_test1199.txt", 200)
-secondaryStructSolver = SecondaryStructSolver()
-secondaryStructSolver.runMLP(trainingFASTAs, testingFASTAs)
+trainingFASTAs = loadSecondaryStructFASTAs("data/seq+ss_train.txt", 1000)
+testingFASTAs = loadSecondaryStructFASTAs("data/seq+ss_test1199.txt", 500)
+secondaryStructSolverMLP(trainingFASTAs, testingFASTAs, [500, 1000])
